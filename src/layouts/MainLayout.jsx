0219fe7b857e5e4e2ebb92209343041e64d33987
Nikesh <nikes.xth@gmail.com>
2025-02-10 08:39:32 +0545

    changes

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router";
 import Footer from "./Footer";
 import NavBar from "./NavBar";
+import { NavigationMenuDemo } from "./NavBarDemo";
 
 export default function MainLayout() {
   return (
     <>
       <NavBar />
+      {/* <NavigationMenuDemo/> */}
       <main>
         <Outlet />
       </main>
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,16 +1,21 @@
 import Hero from "../components/Hero";
 import PrincipalMessage from "../components/PrincipalMessage";
 import Events from "../components/Events";
+import { Button } from "../components/ui/button";
 
 
 export default function Home() {
     return (
         <>
-        <Hero />
+        {/* <Hero />
         <PrincipalMessage />
-        <Events />
-
+        <Events /> */}
+{/* <div>
+      <Button>Click me</Button>
+    </div> */}
         
+
+
         </>
     )
 }

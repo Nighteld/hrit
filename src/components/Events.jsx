64890bf0fb3937d3c820f64d162e0388f3722a9a
Nikesh <nikes.xth@gmail.com>
2025-02-11 19:26:42 +0545

    changes

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -21,7 +21,7 @@ export default function Events() {
     <div className="bg-gray-50 py-12">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <h2 className="text-3xl font-bold text-[#005B7F] mb-8">Important Events</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {events.map((event, index) => (
             <div key={index} className="bg-white rounded-lg shadow-md overflow-hidden">
               <img src={event.image || "/placeholder.svg"} alt={event.title} className="w-full h-48 object-cover" />
@@ -31,7 +31,7 @@ export default function Events() {
               </div>
             </div>
           ))}
-        </div>
+        </div> */}
       </div>
     </div>
   )
--- src/layouts/NavBarDemo.jsx
+++ src/layouts/NavBarDemo.jsx
@@ -1,6 +1,3 @@
-
-import React from "react";
-import { cn } from "@/lib/utils";
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -10,57 +7,106 @@ import {
   NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
+import { cn } from "@/lib/utils";
+import { Twitter } from "lucide-react";
+import React from "react";
 import { Link } from "react-router";
 
 const components = [
   {
-    title: "Alert Dialog",
-    href: "/docs/primitives/alert-dialog",
-    description:
-      "A modal dialog that interrupts the user with important content and expects a response.",
+    title: "About Us",
+    href: "/about-us",
+    description: "Smart Choice for Smart Students",
   },
   {
-    title: "Hover Card",
-    href: "/docs/primitives/hover-card",
-    description: "For sighted users to preview content available behind a link.",
+    title: "Our Mission",
+    href: "/our-mission",
+    description: "Shaping Future Leaders",
   },
   {
-    title: "Progress",
+    title: "Director",
     href: "/docs/primitives/progress",
-    description:
-      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
   },
   {
-    title: "Scroll-area",
+    title: "Principal Message",
+    href: "/docs/primitives/progress",
+  },
+  {
+    title: "Why Hrit",
     href: "/docs/primitives/scroll-area",
-    description: "Visually or semantically separates content.",
   },
   {
-    title: "Tabs",
+    title: "Blogs",
     href: "/docs/primitives/tabs",
-    description:
-      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-  },
-  {
-    title: "Tooltip",
-    href: "/docs/primitives/tooltip",
-    description:
-      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
   },
 ];
 
+const academics = [
+  {
+    title: "Law",
+    href: "/about-us",
+  },
+  {
+    title: "Management",
+    href: "/our-mission",
+  },
+  {
+    title: "Science",
+    href: "/docs/primitives/progress",
+  },
+  {
+    title: "Fine Arts",
+    href: "/docs/primitives/progress",
+  },
+  {
+    title: "Humanities",
+    href: "/docs/primitives/scroll-area",
+  },
+];
+
+const admission = [
+  {
+    title: "Admission Procedure",
+    href: "/about-us",
+  },
+  {
+    title: "Admission Form",
+    href: "/our-mission",
+  },
+  {
+    title: "Scholarship Schemes",
+    href: "/docs/primitives/progress",
+  },
+  {
+    title: "Hotel Management",
+    href: "/docs/primitives/progress",
+  },
+  {
+    title: "Business Studies",
+    href: "/docs/primitives/scroll-area",
+  },
+  {
+    title: "Computer Science",
+    href: "/docs/primitives/scroll-area",
+  },
+];
 
 const navItems = [
   { name: "Home", href: "/" },
-  { name: "HRIT", href: "/academy" ,children:[{
-    name:"About Us",
-    href:"/about-us",
-  },
   {
-    name:"Our Mission",
-    href:"/our-mission",
+    name: "HRIT",
+    href: "/academy",
+    children: [
+      {
+        name: "About Us",
+        href: "/about-us",
+      },
+      {
+        name: "Our Mission",
+        href: "/our-mission",
+      },
+    ],
   },
-] },
   { name: "Admission", href: "/admission" },
   { name: "Alumni Committee", href: "/alumni" },
   { name: "Notices", href: "/event" },
@@ -69,169 +115,162 @@ const navItems = [
   { name: "Downloads", href: "/downloads" },
   { name: "Contact", href: "/articles" },
 ];
-console.log(navItems)
-
+console.log(navItems);
 
 export function NavigationMenuDemo() {
-    
   return (
     <div className="flex justify-between">
-    <NavigationMenu>
-      <NavigationMenuList>
-
-
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-              <li className="row-span-3">
-                <NavigationMenuLink asChild>
-                  <a
-                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                    href="/"
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>HRIT</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+                {components.map((component) => (
+                  <Link
+                    to={component.href}
+                    key={component.title}
+                    legacyBehavior
+                    passHref
                   >
-                    {/* <Icons.logo className="h-6 w-6" /> */}
-                    <div className="mb-2 mt-4 text-lg font-medium">shadcn/ui</div>
-                    <p className="text-sm leading-tight text-muted-foreground">
-                      Beautifully designed components built with Radix UI and Tailwind CSS.
-                    </p>
-                  </a>
-                </NavigationMenuLink>
-              </li>
-              <ListItem href="/docs" title="Introduction">
-                Re-usable components built using Radix UI and Tailwind CSS.
-              </ListItem>
-              <ListItem href="/docs/installation" title="Installation">
-                How to install dependencies and structure your app.
-              </ListItem>
-              <ListItem href="/docs/primitives/typography" title="Typography">
-                Styles for headings, paragraphs, lists...etc
-              </ListItem>
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-
-
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Components</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
-              {components.map((component) => (
-                <Link to={component.href} key={component.title} legacyBehavior passHref>
-                <ListItem key={component.title} title={component.title} to={component.href}>
-                  {component.description}
-                </ListItem>
-                </Link>
-              ))}
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-
-
-        <NavigationMenuItem>
-          <Link to="#" legacyBehavior passHref>
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              Documentation
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-
-
-
-      </NavigationMenuList>
-    </NavigationMenu>
-    <figure>
-  <img src="/hrit.png" alt="Trulli" className="h-20" />
-</figure>
-<NavigationMenu>
-      <NavigationMenuList>
-
-
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-              <li className="row-span-3">
-                <NavigationMenuLink asChild>
-                  <a
-                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                    href="/"
+                    <ListItem
+                      key={component.title}
+                      title={component.title}
+                      to={component.href}
+                    >
+                      {component.description}
+                    </ListItem>
+                  </Link>
+                ))}
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Academics</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+                {academics.map((component) => (
+                  <Link
+                    to={component.href}
+                    key={component.title}
+                    legacyBehavior
+                    passHref
                   >
-                    {/* <Icons.logo className="h-6 w-6" /> */}
-                    <div className="mb-2 mt-4 text-lg font-medium">shadcn/ui</div>
-                    <p className="text-sm leading-tight text-muted-foreground">
-                      Beautifully designed components built with Radix UI and Tailwind CSS.
-                    </p>
-                  </a>
-                </NavigationMenuLink>
-              </li>
-              <ListItem href="/docs" title="Introduction">
-                Re-usable components built using Radix UI and Tailwind CSS.
-              </ListItem>
-              <ListItem href="/docs/installation" title="Installation">
-                How to install dependencies and structure your app.
-              </ListItem>
-              <ListItem href="/docs/primitives/typography" title="Typography">
-                Styles for headings, paragraphs, lists...etc
-              </ListItem>
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
+                    <ListItem
+                      key={component.title}
+                      title={component.title}
+                      to={component.href}
+                    >
+                      {component.description}
+                    </ListItem>
+                  </Link>
+                ))}
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
 
-
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Components</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
-              {components.map((component) => (
-                <Link to={component.href} key={component.title} legacyBehavior passHref>
-                <ListItem key={component.title} title={component.title} to={component.href}>
-                  {component.description}
-                </ListItem>
-                </Link>
-              ))}
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-
-
-        <NavigationMenuItem>
-          <Link to="#" legacyBehavior passHref>
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              Documentation
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-
-
-
-      </NavigationMenuList>
-    </NavigationMenu>
-
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Admission</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+                {admission.map((component) => (
+                  <Link
+                    to={component.href}
+                    key={component.title}
+                    legacyBehavior
+                    passHref
+                  >
+                    <ListItem
+                      key={component.title}
+                      title={component.title}
+                      to={component.href}
+                    >
+                      {component.description}
+                    </ListItem>
+                  </Link>
+                ))}
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link to="#" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Alumni committee
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+      <Link to="/">
+        <figure>
+          <img src="/hrit.png" alt="school-logo" className="h-20" />
+        </figure>
+      </Link>
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <Link to="#" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Notices
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link to="#" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                News
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link to="#" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Gallery
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link to="#" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Downloads
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link to="#" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Contact Us
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
     </div>
   );
 }
 
-const ListItem = React.forwardRef(({ className, title, children, ...props }, ref) => {
-  return (
-    <li>
-      <NavigationMenuLink asChild>
-        <a
-          ref={ref}
-          className={cn(
-            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-            className
-          )}
-          {...props}
-        >
-          <div className="text-sm font-medium leading-none">{title}</div>
-          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-            {children}
-          </p>
-        </a>
-      </NavigationMenuLink>
-    </li>
-  );
-});
+const ListItem = React.forwardRef(
+  ({ className, title, children, ...props }, ref) => {
+    return (
+      <li>
+        <NavigationMenuLink asChild>
+          <a
+            ref={ref}
+            className={cn(
+              "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+              className
+            )}
+            {...props}
+          >
+            <div className="text-sm font-medium leading-none">{title}</div>
+            <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+              {children}
+            </p>
+          </a>
+        </NavigationMenuLink>
+      </li>
+    );
+  }
+);
 
 ListItem.displayName = "ListItem";
